ed89fec89a75efb6e4e566c98bfb7052d3c0e74c
mahmoud hassan <mahmoudhassan10191019@gmail.com>
2025-03-11 20:50:29 +0200

    create naigation screen

---
+++ lib/features/home/presentation/views/navigator_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NavigatorView extends StatelessWidget {
+  const NavigatorView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}

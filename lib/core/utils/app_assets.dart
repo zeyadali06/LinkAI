67371e6c0f26f713138e337e01b0c34c2fb88357
Zeyad Ali <z01551153743@gmail.com>
2025-03-15 17:37:57 +0200

    fix import bug

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-
+  
   /// Assets for animationsLogo
   /// assets/animations/logo.json
   static const String animationsLogo = "assets/animations/logo.json";
@@ -9,8 +9,5 @@ class Assets {
   /// Assets for imagesSplash
   /// assets/images/splash.jpeg
   static const String imagesSplash = "assets/images/splash.jpeg";
-
-  /// Assets for voicesNiceWork
-  /// assets/voices/nice-work.wav
-  static const String voicesNiceWork = "assets/voices/nice-work.wav";
 }
+
